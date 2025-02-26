ae79330a9dcc9b79e0bc03ae76c9d8fd67248a04
ArjobanSingh <arjobansingh1998@gmail.com>
2025-02-27 01:22:11 +0530

    Brute force slow backtrack solution for Array Description

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,74 @@
 use std::{
     cmp,
-    collections::HashMap,
     io::{self, BufWriter, Read, Write},
-    usize,
 };
 
-// const MOD: i32 = 1_000_000_007;
+const MOD: i32 = 1_000_000_007;
 
-fn backtrack(
-    start_idx: usize,
-    books: &Vec<usize>,
-    max_money: usize,
-    mut cur_result: usize,
-    result: &mut usize,
-    prices_map: &mut HashMap<usize, Collection>,
-) {
-    if max_money <= 0 || start_idx >= books.len() {
+fn backtrack(arr: &mut Vec<i32>, idx: usize, u_bound: i32, result: &mut i32) {
+    if idx >= arr.len() {
+        *result = (*result + 1) % MOD;
         return;
     }
 
-    for idx in start_idx..books.len() {
-        let book = books[idx];
-
-        if let Some(collection) = prices_map.get(&book) {
-            if collection.start >= collection.books.len() {
-                continue;
-            }
-
-            // start idx is efficient way to get the next available book with lowest price
-            // as all previous better prices for the book with these pages are used.
-            let price = collection.books[collection.start];
-            if price <= max_money {
-                cur_result += book;
-                *result = cmp::max(*result, cur_result);
-
-                if let Some(collection) = prices_map.get_mut(&book) {
-                    collection.start += 1;
-                }
-
-                // pick this once
-                backtrack(
-                    idx + 1,
-                    books,
-                    max_money - price,
-                    cur_result,
-                    result,
-                    prices_map,
-                );
-
-                cur_result -= book;
-                if let Some(collection) = prices_map.get_mut(&book) {
-                    collection.start -= 1;
-                }
-            }
+    if arr[idx] != 0 {
+        backtrack(arr, idx + 1, u_bound, result);
+    } else if idx == 0 {
+        // at the start
+        let next = arr[idx + 1];
+        if next == 0 {
+            // all of the nums from 1 to u_bound can be used
+            (1..=u_bound).for_each(|num| {
+                arr[idx] = num;
+                backtrack(arr, idx + 1, u_bound, result);
+                arr[idx] = 0;
+            });
+        } else {
+            ((next - 1)..=(next + 1))
+                .filter(|&n| n > 0 && n <= u_bound)
+                .for_each(|num| {
+                    arr[idx] = num;
+                    backtrack(arr, idx + 1, u_bound, result);
+                    arr[idx] = 0;
+                });
         }
-    }
-}
+    } else if idx == arr.len() - 1 || arr[idx + 1] == 0 {
+        let prev = arr[idx - 1];
+        // prev no will never be 0 as per our logic.
+        ((prev - 1)..=(prev + 1))
+            .filter(|&n| n > 0 && n <= u_bound)
+            .for_each(|num| {
+                arr[idx] = num;
+                backtrack(arr, idx + 1, u_bound, result);
+                arr[idx] = 0;
+            });
+    } else {
+        // in middle and next element is not 0
+        let prev = arr[idx - 1];
+        let next = arr[idx + 1];
+        let diff = prev.abs_diff(next);
+        let (start, end) = match diff {
+            0 => (prev - 1, prev + 1),
+            1 => {
+                let smaller = cmp::min(prev, next);
+                let larger = cmp::max(prev, next);
+                (smaller, larger)
+            }
+            2 => {
+                let smaller = cmp::min(prev, next);
+                (smaller + 1, smaller + 1)
+            }
+            _ => return,
+        };
 
-struct Collection {
-    books: Vec<usize>,
-    start: usize,
+        (start..=end)
+            .filter(|&n| n > 0 && n <= u_bound)
+            .for_each(|num| {
+                arr[idx] = num;
+                backtrack(arr, idx + 1, u_bound, result);
+                arr[idx] = 0;
+            });
+    }
 }
 
 fn main() {
@@ -72,7 +81,7 @@ fn main() {
     let mut lines = input.lines();
 
     let mut out = BufWriter::new(io::stdout().lock());
-    let input: Vec<usize> = lines
+    let input: Vec<i32> = lines
         .next()
         .expect("Error reading n")
         .split_ascii_whitespace()
@@ -80,43 +89,24 @@ fn main() {
         .collect();
 
     // let n = input[0];
-    let x = input[1];
+    let u_bound = input[1]; //  upper bound
 
-    let prices: Vec<usize> = lines
+    let mut items: Vec<i32> = lines
         .next()
         .expect("Error reading prices")
         .split_ascii_whitespace()
         .filter_map(|it| it.parse().ok())
         .collect();
 
-    // pages to prices map desc
-    let mut prices_map: HashMap<usize, Collection> = HashMap::with_capacity(x);
-    let mut books: Vec<usize> = Vec::with_capacity(x);
-
-    for (idx, book) in lines
-        .next()
-        .expect("Error getting pages")
-        .split_ascii_whitespace()
-        .enumerate()
-    {
-        let book: usize = book.parse().expect("Error parsing pages");
-        let collection = prices_map.entry(book).or_insert(Collection {
-            books: vec![],
-            start: 0,
-        });
-
-        match collection.books.binary_search(&prices[idx]) {
-            Ok(pos) | Err(pos) => collection.books.insert(pos, prices[idx]),
-        }
-
-        books.push(book);
-    }
-    books.sort_by(|a, b| b.cmp(a));
-
     let mut result = 0;
-    backtrack(0, &books, x, 0, &mut result, &mut prices_map);
+    if items.len() == 1 {
+        writeln!(out, "{}", if items[0] == 0 { u_bound } else { 1 }).expect("error writing line");
+        out.flush().ok();
+        return;
+    }
 
-    writeln!(out, "{}", result).expect("error writing line");
+    backtrack(&mut items, 0, u_bound, &mut result);
 
+    writeln!(out, "{result}").expect("error writing line");
     out.flush().ok();
 }
